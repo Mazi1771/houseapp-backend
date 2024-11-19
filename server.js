a826101a6ddf955625e1b2dbde694ee0745055dc
Mazi1771 <mazi1771@gmail.com>
2024-11-19 15:14:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/api/properties', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Serwer działa na porcie ${PORT}`);
+const port = process.env.PORT || 10000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Serwer działa na porcie ${port}`);
 });
